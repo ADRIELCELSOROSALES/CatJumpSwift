d15eeaddeda774b740508bb35c81478726fdfa81
Adriel Rosales <adriel.rosales@encodelabs.com.ar>
2026-05-04 16:22:48 -0300

    Initial Commit

---
+++ CatJump/CatJumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatJumpApp.swift
+//  CatJump
+//
+//  Created by Adriel Celso Rosales on 04/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct CatJumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
